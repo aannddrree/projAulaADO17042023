de5341ccd5c56738473a76b4feb65f44ff986357
= <andre.silva@hubdoctor.com.br>
2023-04-19 15:27:09 -0300

    API

---
+++ API.Models/Engine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Engine
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string Description { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return "Descrição: " + Description;
+        }
+        #endregion
+    }
+}
---
+++ API/Controllers/AirplaneController.cs
@@ -0,0 +1,19 @@
+﻿using API.Models;
+using API.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AirplaneController : ControllerBase
+    {
+
+        [HttpPost(Name = "Insert")]
+        public bool Insert(Airplane airplane)
+        {
+            return new AirplaneService().Insert(airplane);
+        }
+    }
+}
